4a3235a197d09f706a7d4b445d358da7c9e7c341
robonexx <robertwagar@gmail.com>
2022-05-04 09:30:19 +0200

    did the key prop in the wrong file  hade to pass it as a prop to the component

--- src/components/responsiveGrid/GridItem.js
+++ src/components/responsiveGrid/GridItem.js
@@ -6,19 +6,19 @@ import styles from '../../styles/components/ResponsiveGrid.module.scss';
 
 export default function GridItem(props) {
   return (
-
-        <div className={styles.item}>
-          <div className={styles.title}>{props.title}</div>
-          <div className={styles.image}>
-            <Image
-              src={`/images/${props.img}`}
-              width={400}
-              height={300}
-              alt='item images'
-            />
-          </div>
-          <Link className={styles.link} href={props.url}>Read More...</Link>
-        </div>
-    
+    <div className={styles.item} key={props.idx}>
+      <div className={styles.title}>{props.title}</div>
+      <div className={styles.image}>
+        <Image
+          src={`/images/${props.img}`}
+          width={400}
+          height={300}
+          alt='item images'
+        />
+      </div>
+      <Link className={styles.link} href={props.url}>
+        Read More...
+      </Link>
+    </div>
   );
 }
--- src/pages/timeline/index.js
+++ src/pages/timeline/index.js
@@ -15,10 +15,8 @@ export default function Timeline() {
           This Page is still Just a test for layouts and grid
         </h1>
         <ResponsiveGrid>
-          {items.map(({ title, img, url }, i) => (
-            <li key={i}>
-              <GridItem title={title} img={img} url={url} />
-            </li>
+          {items.map(({ title, img, url }, idx) => (
+            <GridItem title={title} img={img} url={url} idx={idx} />
           ))}
         </ResponsiveGrid>
         <Section>Section</Section>
