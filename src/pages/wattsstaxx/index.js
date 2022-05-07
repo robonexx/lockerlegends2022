2902dfab9c3255e04b8025dedafc152ad08dc64b
robonexx <robertwagar@gmail.com>
2022-05-07 16:03:54 +0200

    Have to upload and try if this works, slider with framer motion, will separate it and make it a component if Ill use it

--- src/pages/wattsstaxx/index.js
+++ src/pages/wattsstaxx/index.js
@@ -1,40 +1,65 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import styles from '../../styles/pages/Wattsstaxx.module.scss';
 import { getPostCategoryInterviews } from '../../lib/posts/get-posts';
+import Image from 'next/image';
+import { formatDate } from '../../utils/functions';
 
 export default function index({ interviews }) {
-    
-    const images = []
+  const [width, setWidth] = useState(0);
 
-    interviews.map(({node: {featuredImage} }) => {
-        images.push(featuredImage.node.sourceUrl)
-    })
+  const slider = useRef();
 
-    console.log(images)
+  const images = [];
+
+  interviews.map(({ node: { featuredImage, title, date } }) => {
+    images.push(featuredImage.node.sourceUrl);
+  });
+
+  useEffect(() => {
+    setWidth(slider.current.scrollWidth - slider.current.offsetWidth);
+  }, []);
+
+  console.log(images);
   return (
     <div className={styles.wattsstaxx}>
       <div className={styles.inner}>
-              <motion.h1 animate={{ x: 0 }}
-              initial={{x: 250}}>Slider</motion.h1>
-          </div>
-          <motion.div className={styles.slider}>
-
-              <motion.div className={styles.inner_slider}>
-
-              </motion.div>
-
-          </motion.div>
+        <motion.h1 animate={{ x: 0 }} initial={{ x: 250 }}>
+          Slider
+        </motion.h1>
+      </div>
+      <motion.div className={styles.slider} ref={slider}>
+        <motion.div
+          drag='x'
+          dragConstraints={{ right: 0, left: -width }}
+          whileDrag={{ scale: 0.9 }}
+          className={styles.inner_slider}
+        >
+          {/* {images.map((image) => ( */}
+          {interviews.map(({ node: { featuredImage, title, date } }) => (
+            <motion.div className={styles.slider_item} key={title}>
+              <Image
+                src={featuredImage.node.sourceUrl}
+                layout='fill'
+                objectFit='cover'
+                objectPosition='top center'
+                alt='blog post image'
+                priority
+              />
+              <h3 className={styles.slider_title}>{title}</h3>
+              <span>{formatDate(date)}</span>
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
 
-
 export async function getStaticProps() {
-    const interviews = await getPostCategoryInterviews();
-  
-    return {
-      props: { interviews: interviews.edges },
-    };
-  }
-  
+  const interviews = await getPostCategoryInterviews();
+
+  return {
+    props: { interviews: interviews.edges },
+  };
+}
