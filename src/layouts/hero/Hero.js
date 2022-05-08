b1bd8fdc6906d5e3952fbdfbd73db3a5fc5dee4f
robonexx <robertwagar@gmail.com>
2022-05-08 09:21:56 +0200

    fixed hero title, changed overlay of video to be a little dimmed. changed background to a darker black

--- src/layouts/hero/Hero.js
+++ src/layouts/hero/Hero.js
@@ -9,7 +9,7 @@ export default function Hero() {
         <source src='/video/la.webm' type='video/webm' />
       </video>
       <section className={styles.hero_wrapper}>
-        <h3 className={styles.hero_pretitle}>Welcome to</h3>
+        <h3 className={styles.hero_pretitle}></h3>
         <h1 className={styles.hero_title}>LOCKERLEGENDS</h1>
 
         <h3 className={styles.hero_subtitle}>
