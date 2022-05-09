234dca8e8c04bdbf7253cacae1d3f6c4a49ab7df
robonexx <robertwagar@gmail.com>
2022-05-09 22:31:33 +0200

    nope, dident work, so back to what it was before

--- src/layouts/hero/Hero.js
+++ src/layouts/hero/Hero.js
@@ -2,27 +2,15 @@ import React from 'react';
 import Image from 'next/image';
 import styles from '../../styles/layout/Hero.module.scss';
 
-// trying this workaround to play videos on iphone / safari
-
-const AutoPlayvideo = () => {
-  <div
-    dangerouslySetInnerHTML={{
-      __html: (
-        <video autoPlay loop muted playsInline>
-          <source src='/video/la.webm' type='video/webm' />
-        </video>
-      ),
-    }}
-  />;
-};
-
 export default function Hero() {
   return (
     <div className={styles.hero}>
       <div className={styles.hero_overlay}></div>
-      <div className={styles.hero_video}>
-        <AutoPlayvideo />
-      </div>
+
+      <video autoPlay loop muted playsInline className={styles.hero_video}>
+        <source src='/video/la.webm' type='video/webm' />
+      </video>
+
       <section className={styles.hero_wrapper}>
         {/*  <h3 className={styles.hero_pretitle}></h3> */}
         {/*  <h1 className={styles.hero_title}>LOCKERLEGENDS</h1> */}
