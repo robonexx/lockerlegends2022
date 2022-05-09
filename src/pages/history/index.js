7666062cd3cfd33e6066e8a4e145e8677bf10e41
robonexx <robertwagar@gmail.com>
2022-05-09 14:25:40 +0200

    history page

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import Content_container from '../../layouts/pagelayout/Content_container';
 import LeftSide from '../../layouts/pagelayout/LeftSide';
 import PageLayout from '../../layouts/pagelayout/PageLayout';
@@ -8,26 +9,338 @@ import styles from '../../styles/pages/History.module.scss';
 
 export default function History() {
   return (
-    <div>
+    <div className={styles.history}>
       <PageLayout>
         <Content_container>
-          {' '}
           <div className={styles.info_container}>
             <h1>OUR STORY OF THE HISTORY OF LOCKING</h1>
-            <h2>THIS IS A HISTORY OF THE ORIGINAL FOUNDATION OF A STREET DANCE CALLED ”LOCKING”</h2>
+            <h2>
+              THIS IS A HISTORY OF THE ORIGINAL FOUNDATION OF A STREET DANCE
+              CALLED ”LOCKING”
+            </h2>
             <p>
               You can click on the images to go that specific page and read more
             </p>
           </div>
         </Content_container>
-        <section>
-          <LeftSide>
-            <div className={styles.left_container}>
-            <p>:as seen through the eyes of some of the original pioneers</p>
+        <LeftSide>
+          <div className={styles.left_container}>
+            <p className={styles.para_color}>
+              :as seen through the eyes of some of the original pioneers
+            </p>
+            <p>
+              NOTE: History is a compilation of historical events, most history
+              is passed down legendary information, as seen from various
+              perspectives, some are documented information passed down through
+              writings or first hand eye witnesses. This information has been
+              the results of first hand eye witnesses, and original creators and
+              contributors from the ORIGINAL locking era. There are various
+              historical accounts of various events in the 4 decades of Lockings
+              existence. But there is only ONE Original era, which was seen
+              through the many eyes that experienced it, which all came together
+              to produce its foundation ..Read and Enjoy
+            </p>
+          </div>
+        </LeftSide>
+        <RightSide>
+          <div className={styles.right_container}>
+            <div className={styles.image}>
+              <Image
+                src='/images/gogobrotherswatts.png'
+                width='100'
+                height='100'
+                layout='responsive'
+                objectFit='cover'
+                objectPosition='center'
+              />
             </div>
-          </LeftSide>
-          <RightSide>Right</RightSide>
-        </section>
+          </div>
+        </RightSide>
+        <LeftSide>
+          <div className={styles.left_container}>
+            <p>
+              Locking or Campbellocking is a dance art form with improvisational
+              move called the lock. These were created by Don Campbell and put
+              to a specific rhythm and style in the nightclubs of Los Angeles in
+              the early 1970s.
+            </p>
+          </div>
+        </LeftSide>
+        
+        <RightSide>
+          <div className={styles.right_container}>
+            <p>
+              This dance and subculture quickly caught on and was soon the rage
+              of a new television dance show called Soul Train. Individual
+              dancers displayed quick locking and pointing movements along with
+              hand slaps and splits.
+            </p>
+          </div>
+        </RightSide>
+        <LeftSide>
+          <div className={styles.left_container}>
+            <Image
+              src='/images/donflying.jpg'
+              width='100'
+              height='100'
+              layout='responsive'
+              objectFit='cover'
+              objectPosition='center'
+            />
+          </div>
+        </LeftSide>
+        <LeftSide>
+          <div className={styles.left_container}>
+            <p>Don “Campbellock” Campbell early soul Train Line</p>
+            <p>
+              Don and his partner, Damita Jo Freeman toured with the Soul Train
+              Gang. Shortly after, Don comprised a group of improvisational solo
+              style locking dancers (Each dancer would step out do his solo and
+              step back in line) keeping time in the background was the only
+              source of synchronized dancing this group of solo artist did. They
+              were comprised of Don Campbell, Fred “Mr. Penguin” Berry, Charles
+              Robot, Slim Robot and Sambo Lock. They were called The “
+              Campbellock Dancers“.
+            </p>
+          </div>
+        </LeftSide>
+        <RightSide>
+          <div className={styles.right_container}>
+            <p>
+              Don Campbell along with dancers like Greg “Campbell Jr” Pope,
+              Jimmy “Scooby doo” Foster, Fred “Mr. Penguin” Berry ( AKA
+              Rerun),Damita Jo Freeman, The Go-Go- Brothers (Tony Lewis and
+              Edwin “Buddy” Lombard),Kevin “YoYo”Lombard, James “Skeeter Rabbit”
+              Higgins, Leo “Fluky Luke” Williamson, Johnny “Sambo Lock”
+              McCloud,Damita Jo Freeman, Charles “Robot” Washington, Bill “Slim
+              Robot” Williams, Alpha Anderson, Fredie Maxie, Guy “Shocklock”
+              Evans, Tito Collins, Jeffery Daniels, Arnetta “Netta Bug” Johnson.
+              Eugine “F & M Henderson and SEVERAL other old school originals
+              would meet in nightclubs like, ” The Citadel” in Hollywood, The
+              Summit on the Hill or Mavericks Flats and share steps and moves.
+              after the initial years locking when began to grow, Dancers like
+              Adolfo “Shabba doo” Quinones , Hayword “Tito Collins , Mike “Peek
+              a boo” Frenke and Freddi Maxie and a few Others joined the
+              original club scene
+            </p>
+          </div>
+        </RightSide>
+       
+        <LeftSide>
+          <div className={styles.right_container}>
+            <Image
+              src='/images/mavericksflat.png'
+              width='100'
+              height='100'
+              layout='responsive'
+              objectFit='cover'
+              objectPosition='center'
+            />
+          </div>
+        </LeftSide>
+
+        <RightSide>
+          <div className={styles.left_container}>
+            <p>
+              During that era & time, Greg Campbell Jr. Pope “Scooby doo”
+              Foster, “, along with the GoGo ,YoYo CoCo brothers Family of Tony
+              and Buddy Go-Go and Skeeter Rabbit, Arnetta Johnson . Kevin “yoYo”
+              Lombard. sometimes partying with Leo “Fluky Luke” Williamson &
+              Fred Berry at house parties, high schools , picniks and social
+              events creating steps that were performed in unison. A movement
+              towards group dancing was taking place in Watts,Compton & South
+              Central, Los Angeles.and then continued interacting with other
+              street dancers Like Greg “Captain Crunch” Dandridge & Steve
+              “Sinbad Crane” This was evolution of the STREET MOVEMENT OF
+              LOCKING
+            </p>
+          </div>
+        </RightSide>
+       
+        <div className={styles.container}>
+          <p>
+            In 1972 The Go-Go brothers (Tony and Buddy along with the Original
+            Skeeter Rabbitt & Kevin YoYo ) combined to form the first
+            synchronized locking dance group; performing skits and routines in
+            both large arenas as well as small high school functions.
+            <br />
+            In late 1972, early 1973 Scooby Doo and Greg Campbellock Jr.
+            combined with the Go- Go Brothers (Tony & Buddy), Skeeter Rabbitt
+            (OG) and with female lockers Arnetta Johnson, Lorna Dune & Shelly
+            Cepeda formed the first unisex Locking group called, “Creative
+            Generation“. This group studied dance, taught, performed and toured
+            as members of the Legendary Watts Writers Workshop. Later that
+            summer,Is when Don Campbell along with Toni Basil organized and
+            introduced the group, “THE LOCKERS”( originally named the
+            campbellock dancers after Don Campbell’s first group). “The Lockers”
+            was one of the most dynamic, influential groups in the history of
+            the dance, comprising both improvisational dancers and synchronized
+            dancers to form a group who could attract the culturally diverse
+            ,COMMERCIAL audiences
+            <br />
+            <br />
+            Greg “Cambellock Jr” Pope , Had left the Watts writers first group
+            “Creative Generation” and helped introduced to the group (The
+            Lockers), synchronized locking choreography, which he had learned
+            during his Creative Generation experience, at the Watts Writers
+            Workshop. Him along with Don Campbell, “Slim Robot”, Fred “Rerun”
+            Berry, Fluky Luke, Trained Ballerina & Chorographer Toni Basil,
+            along with a young new talent, from Chicago Adolfo “Shabba Doo”
+            Quinones. The Dance style of Locking went from just a style to a
+            performance artform, and was NOW revealed to the world.
+            <br />
+            <br />
+            During the Original groups existence, in the early 70’s. Other OG
+            dancers such as Tony Go-Go and “The Original Skeeter Rabbit” (James
+            Higgins)were members who were added and danced as OFFICIAL
+            ALTERNATES with the Original Official Group The Lockers,
+            <br />
+            <br />
+            Many dancers have claimed original status ,some even taking pictures
+            at events at mavericks today , or at Soul Train reunion Parties, and
+            THEY ARE NOT. They may have danced on soul train or went to the
+            clubs AFTER that original era , but they want you to think they were
+            there right along with the REAL OG’s & they were not original. YES
+            they may have been good, and may have done great commercial stuff
+            ,but many of them were not back there on that original scene ,when
+            locking FIRST developed. we have mentioned most who was & we
+            clarified most who were around durng other time periods. if you were
+            on the ORIGINAL scene and are not mentioned we would be happy to
+            include you , If you want to know who was and who wasn’t just ask ,
+            we will post and clarify your confusion… ORIGINAL means ORIGINAL,
+            <br />
+            <br />
+            Please take note : All the way up into the 80s the group The LOCKERS
+            was reconstructed many times with various dancers added or dropped
+            as members.( see some of the later groups)As the group went through
+            many personal & professional transitions, many of the dancers added
+            in that time span , were not original era dancers and were not added
+            on an OFFICIAL level, but they did dance with various original
+            members under the assumption they were dancing with “THE ORIGINAL
+            LOCKERS” BEWARE of all of those claiming to have been there !
+            <br />
+            <br />
+            Also, during the early to mid 70s period other urban groups such as
+            33RPM and the Ghetto Dancers were also formed (see acknowledgement
+            page). These were comprised of various combinations of other old
+            school locking (dancers) pioneers. Once the dance was introduced on
+            National Television, It set off a dance phenomenon which now began
+            spreading from the Los Angeles inner city, to the once segregated
+            Suburbs, of Orange County the once segregated surrounding areas,(
+            this is how other dancers of different generation came about) And
+            eventually the style was seen and began to be imitated across the
+            Country. Some of these newly found admirers of the style found
+            themselves exposed to the ORIGINAL movement and Identified with the
+            Original Foundation, Because the Dance style of Locking had ONLY
+            been exposed through site.The Original Culture ,History and
+            Foundation was only passed on to those that lived or sought the
+            inner city experience.The discovery of the Newly discovered “Dance
+            style” enticed supburban groups, promoters, dancers and managers,
+            who were normally segregated to flood the scene . Some were driven
+            by the love, facination and respect for the artform. Others simply
+            ignored any foundatition and only disired to exploit an artform to a
+            commercial market. Many of these dancers and groups, who bypassed
+            the Foundation to simply imitate the moves as they thought they saw
+            them, they were known by the real street sub-culture simply as “SLOP
+            lockers”.
+            <br />
+            <br />
+            Note: Many of these groups and individules quickly clammered to
+            exploit not just the artform of locking but it’s entire culture and
+            roots. Many renamed steps and plagerized entire taped routines. Some
+            never learned the internal rules of the sub-culture. They never
+            learned or inquired about the brotherhood which made locking great.
+            They only attemped to capitalize, exploit and alter the history of
+            locking.
+            <br />
+            <br />
+            Many don’t understand what the History &; background has to do with
+            the dance of Locking, Many wonder why it just can’t be a dance
+            style, show or circus act..WATCH this video WATTS STAX and see what
+            the inspiration was and still is for those that KEEP IT REAL
+            <br />
+            <br />
+            By 1973 there were an excess of pioneers known as lockers who made
+            their name and reputations in the urban communities and night clubs.
+            The few known as the Original members of “The Lockers Group” became
+            the internationally famous. But ORIGINAL Dancers Like Guy
+            “Shocklock” Evans, Jeffery “Cat Man” Macintire, Eugene “F&M”
+            Henderson, Roy “Lie Lock”, “Sneaky T”, Enock “Elmer Fudd”, John
+            “Okiedoke” Wilson “Elmer Fud’ Smith (RIP), , Steve “Sinbad” Crane,
+            Arnetta “Nettabug” Johnson,Greg “Capt Crunch” Dandridge, Fredi Maxie
+            and several others were some of the Street legends, who contributed.
+            <br />
+            <br />
+            By the mid 1970s locking had now been seen by many, and was now
+            spreading. Southern California now had the Originating movement, but
+            the dance now had migrated from the Inner city to suburban areas,
+            such as Orange County California, where some of the originals were
+            now accepted and shared the dance style with locals in that area,
+            and to the Ventura Valley where those that seen an imitated the
+            dance style open the doors to other eyes. But the Original style
+            continued to develop in the Southern California Hoods of Watts,
+            South Central , Compton and Inglewood, with Other 80s groups and Old
+            School legends who bridged the gap between Locking and Popping
+            <br />
+            <br />
+            There were many who danced as members of the group “The Lockers” and
+            many more who are refered to as Lockers by virtue of the dance of
+            locking, which they have represented
+            <br />
+            <br />
+            In 1974 Jimmy “Scoobydoo Foster toured with a group called
+            “Something Special’ introducing Locking along with various other
+            dances internationally , into Japan,
+            <br />
+            <br />
+            By 1975 the artform had evolved both socially and professionally and
+            in the late 1970’s, “The Original Lockers Group disbanded” and a
+            third generation was formed in the mid 1980’s, adding old school
+            pioneer “Alpha“Anderson,(who created the alpha floor move) along
+            with old schoolers from the mid 70s, Lewis “Deputy” Green, Lionel
+            “Big D” Douglas“and Anna “Lollipop” Sanchez, and 2nd generation GoGo
+            brothers Manny “Loose Caboose” Tristin this formed the third
+            generation “LOCKERS” Group. In the mid 1970’s Jimmy Scooby do Foster
+            and a Group called “Something Special “was the first to
+            internationally expose Asia to Locking along with other Cultural
+            social dances. In the Late 70s Watts groups, such as the second
+            generation of Kaptin Krunch (Charles Washington of the FBs) and his
+            Funky Bunch followed the legacy of the Inner City Streets, by
+            performing on Soul Train and in the 80s and as members of the
+            Lockers in a 1984 performance, dancing both Locking & Popping. By
+            the mid 1980’s, Tony Go-Go Lewis established a school re-introducing
+            the locking dance art in Japan. Today, there is a wave of interest
+            to know and understand the history and sub-culture of a true dance
+            phenomenon all over the world.
+            <br />
+            <br />
+            By no means were these the only groups or contributors ( There are
+            so many) to this dance and subculture that helped to form the multi
+            style and cultures of today’s hip hop History of anything begans
+            with an inspiration and an action, The inspiration of the artform
+            came from a struggle of a people, the and action was put into place
+            by Don Campbell, The early history stims from this one person
+            through a group of early contributors to a specific area at a
+            specific time in history,The innercity of South central Los Angeles,
+            Watts & Compton California will always be that inception point in
+            history, the issues surrounding those times will always be the
+            inspiration.from there it insited a streetdance movement with
+            purpose and pride. Today when one speaks of Locking history it can
+            include many points of exposure to the style and all create an ever
+            expanding chapter Many thanks to Don Campbell and the many other Old
+            and New School pioneers.
+            <br />
+            <br />
+            Many ask why can’t Locking just be an act like the circus, why can’t
+            it just be entertainment , well it can be entertainment, but not
+            like clowns, or minstril , it means much more check out the video
+            below, soul and funk music linked to what the influences was that
+            made this dance great
+            <br />
+            <br />
+            Any Questions.. please send us an email at info@lockerlegends.net .
+          </p>
+        </div>
       </PageLayout>
     </div>
   );
--- src/pages/terminology/index.js
+++ src/pages/terminology/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import Image from 'next/image'
 import Content_container from '../../layouts/pagelayout/Content_container';
 import PageLayout from '../../layouts/pagelayout/PageLayout';
+import LeftSide from '../../layouts/pagelayout/LeftSide';
+import RightSide from '../../layouts/pagelayout/RightSide';
+
+// styles
+import styles from '../../styles/pages/Terminology.module.scss';
 
 export default function Terminology() {
   return (
-    <div>
+    <div className={styles.terminology}>
       <PageLayout>
         <Content_container>
-          <h1>Street Terminology</h1>
+          <div className={styles.info_container}>
+            <h1>Street Terminology</h1>
+            <h2>text</h2>
+            <p>text</p>
+          </div>
         </Content_container>
+        <LeftSide>
+          <div className={styles.left_container}>
+            <p>text</p>
+          </div>
+        </LeftSide>
+        <RightSide>
+          <p>Right side</p>
+        </RightSide>
+       
       </PageLayout>
     </div>
   );
