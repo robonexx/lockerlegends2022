ff037de72353d4b9e104dd39a2995faa16b102f2
robonexx <robertwagar@gmail.com>
2022-05-09 22:20:18 +0200

    trying out a workaround to play hero video on iphones

--- src/layouts/hero/Hero.js
+++ src/layouts/hero/Hero.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import Image from 'next/image'
+import Image from 'next/image';
 import styles from '../../styles/layout/Hero.module.scss';
+
+// trying this workaround to play videos on iphone / safari
+
+const AutoPlayvideo = () => {
+  <div
+    dangerouslySetInnerHTML={{
+      __html: (
+        <video autoPlay loop muted playsInline className={styles.hero_video}>
+          <source src='/video/la.webm' type='video/webm' />
+        </video>
+      ),
+    }}
+  />;
+};
 
 export default function Hero() {
   return (
     <div className={styles.hero}>
       <div className={styles.hero_overlay}></div>
-      <video autoPlay loop muted playsInline className={styles.hero_video}>
-        <source src='/video/la.webm' type='video/webm' />
-      </video>
+      <AutoPlayvideo />
       <section className={styles.hero_wrapper}>
-       {/*  <h3 className={styles.hero_pretitle}></h3> */}
-       {/*  <h1 className={styles.hero_title}>LOCKERLEGENDS</h1> */}
+        {/*  <h3 className={styles.hero_pretitle}></h3> */}
+        {/*  <h1 className={styles.hero_title}>LOCKERLEGENDS</h1> */}
         <div className={styles.hero_logo}>
-          <Image src='/svg/SVG_LOGO_TEXT.svg'
-            layout='fill'
-            alt='logo'
-          ></Image>
+          <Image src='/svg/SVG_LOGO_TEXT.svg' layout='fill' alt='logo'></Image>
         </div>
 
         <h3 className={styles.hero_subtitle}>
