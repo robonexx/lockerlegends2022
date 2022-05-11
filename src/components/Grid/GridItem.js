9b78153d09c41989d5418bd83684f6db5cdf0e0e
robonexx <robertwagar@gmail.com>
2022-05-11 17:49:36 +0200

    My attempt to fix the featured image being empty dident work, need to fix a work around.

--- src/components/Grid/GridItem.js
+++ src/components/Grid/GridItem.js
@@ -18,28 +18,6 @@ export default function GridItem({
 }) {
   return (
     <motion.div className={styles.grid_item}>
-      {/*  <Image
-        src={sourceUrl}
-        layout='fill'
-        objectFit='cover'
-        objectPosition='top center'
-        alt='blog post image'
-        priority
-      /> */}
-
-      {/* Checking if there is an featured image for the post otherwise insert an other photo */}
-      {sourceUrl === null ? (
-        <div className={styles.image}>
-          <Image
-            src={`/images/interview.webp`}
-            layout='fill'
-            objectFit='cover'
-           
-            alt='blog replacement image'
-            priority
-          />
-        </div>
-      ) : (
         <div className={styles.image}>
           <Image
             src={sourceUrl}
@@ -50,8 +28,6 @@ export default function GridItem({
             priority
           />
         </div>
-      )}
-
       <h3 className={styles.grid_title}>{title}</h3>
       <Link href={`/blog/` + slug} passHref>
         <a aria-label={title}></a>
