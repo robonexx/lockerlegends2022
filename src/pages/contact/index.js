b203f9f82207ce538d1eb1595c7fd07d4a3b1854
robonexx <robertwagar@gmail.com>
2022-05-13 13:20:56 +0200

    contact

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import PageLayout from '../../layouts/pagelayout/PageLayout';
+
+// styles
+import styles from '../../styles/pages/Contact.module.scss'
 
 export default function Contact() {
   return (
-    <div>
-      <PageLayout>
-        
-          <h1 style={{ color: 'white', fontSize: '4rem' }}>Contact page</h1>
+    <div className={styles.contact}>
+     
+        <h2>This page will be updated shortly</h2>
+      <h1>CONTACT US!</h1>
+      <h2>OG SKEETER RABBIT</h2>
+      <p>skeeterrabbit@lockerlegends.net</p>
+      <h1> </h1>
+      <h2>If you have issues with the website or any questions about the site</h2>
+      <p>robertwagar@gmail.com</p>
+      <h2></h2>
       
-      </PageLayout>
+     
     </div>
   );
 }
