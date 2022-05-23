ced466f05b87dc606a7c103247eded3058aff099
robonexx <robertwagar@gmail.com>
2022-05-23 18:21:02 +0200

    footer

--- src/layouts/footer/Footer.js
+++ src/layouts/footer/Footer.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import { useEffect } from 'react';
 
-import styles from '../../styles/layout/Footer.module.scss'
+import styles from '../../styles/layout/Footer.module.scss';
+
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <h2>Footer</h2>
+      <p>
+        &copy; Copyright {new Date().getFullYear()}. All Rights Reserved.
+        LOCKERLEGENDS
+      </p>
+      <img src='/images/roblogo.png' alt='logo robertwagar@gmail.com' type='png' />
     </footer>
   );
 }
